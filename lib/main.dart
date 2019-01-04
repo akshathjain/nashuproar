dd9ba36811f83e2edc6d5b718152a948e572ce42
Akshath Jain <akshath.r.jain@gmail.com>
2019-01-04 18:59:23 -0500

    updated readme

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,20 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.yellow.shade700,
         accentColor: Colors.black
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
