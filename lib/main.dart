12a2f50b17a18213c34506e34fb1d8e5799a2326
Akshath Jain <akshath.r.jain@gmail.com>
2019-01-04 21:50:28 -0500

    finished very basic article view

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+void main() => runApp(NASHUproar());
+
+class NASHUproar extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -14,53 +18,102 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.yellow.shade700,
         accentColor: Colors.black
       ),
-      home: HomePage(title: 'Flutter Demo Home Page'),
+      home: HomePage(),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  HomePage({Key key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return new ArticleView(
+      url: "https://nashuproar.org/wp-json/wp/v2/posts/13407",
+    );
+  }
+}
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+class ArticleView extends StatefulWidget{
+  ArticleView({Key key, this.url}) : super(key: key);
+  final String url;
+
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView>{
+  Map _info; //the info about the article
+
+  @override
+  void initState() {
+    super.initState();
+    print("hello");
+    print(widget.url);
+    fetchArticleInfo(widget.url).then((Map m){
+      setState(() {
+        print("hello");
+        print(m);
+        _info = m;
+        print(m["title"]["rendered"]);
+      });
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text("NASH Uproar"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: _createBody(),
     );
+  }
+
+  Widget _createBody(){
+    //Case: the information still needs to be loaded
+    if(_info == null){
+      return new Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    
+    //Case: the information has already loaded
+    return new ListView(
+      children: <Widget>[
+        Text(_info["title"]["rendered"]), //title
+        Text(_getDate(DateTime.parse(_info["date"]))), //date
+        Text("Author"), //author
+        Text(_info["content"]["rendered"]), //article content
+      ],
+    );
+  }
+
+  String _getDate(DateTime dt){
+    List months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return months[dt.month - 1] + " " + dt.day.toString() + ", " + dt.year.toString(); 
+  }
+
+  Future<Map> fetchArticleInfo(String url) async{
+    final respone = await http.get(url);
+    return json.decode(respone.body);
   }
 }
