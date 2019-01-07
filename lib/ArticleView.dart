d1335d11927610408c339868e0cb9f5ccfb28c14
Akshath Jain <akshath.r.jain@gmail.com>
2019-01-07 15:37:58 -0500

    fixed typo in categories.json

---
+++ lib/ArticleView.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ArticleView extends StatefulWidget{
+  ArticleView({Key key, this.url}) : super(key: key);
+  final String url;
+
+  @override
+  _ArticleViewState createState() => _ArticleViewState();
+}
+
+class _ArticleViewState extends State<ArticleView>{
+  Map _info; //the info about the article
+
+  @override
+  void initState() {
+    super.initState();
+    fetchArticleInfo(widget.url).then((Map m){
+      setState(() {
+        _info = m;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: Text("NASH Uproar"),
+      ),
+      body: _createBody(),
+    );
+  }
+
+  Widget _createBody(){
+    //Case: the information still needs to be loaded
+    if(_info == null){
+      return new Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    
+    //Case: the information has already loaded
+    return new ListView(
+      children: <Widget>[
+        CachedNetworkImage(
+          imageUrl: _info["_embedded"]["wp:featuredmedia"][0]["media_details"]["sizes"]["medium"]["source_url"],
+        ),
+        Html(
+          data: _info["title"]["rendered"],
+        ), //title
+        Text(_getDate(DateTime.parse(_info["date"]))), //date
+        Text(_info["_embedded"]["author"][0]["name"]), //author
+        Html(
+          data: _info["content"]["rendered"],
+          customRender: (node, children){
+            if (node is dom.Element) {
+                switch (node.localName) {
+                  case "iframe":
+                    return new Container(
+                    width: MediaQuery.of(context).size.width * .75,
+                    child: Card(
+                      child: InkWell(
+                        onTap: () => _openYoutube(node.attributes["src"]),
+                        child: Stack(
+                          alignment: Alignment.center,
+                          children: <Widget>[
+                            CachedNetworkImage(
+                              width: MediaQuery.of(context).size.width * .75,
+                              imageUrl: "https://img.youtube.com/vi/" + node.attributes["src"].split("/")[node.attributes["src"].split("/").length - 1] + "/1.jpg",
+                              fit: BoxFit.cover,
+                            ),
+                            Icon(Icons.play_circle_filled, color: Colors.white,),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                }
+              }
+          },
+        ), //article content
+      ],
+    );
+  }
+
+  String _getDate(DateTime dt){
+    List months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    return months[dt.month - 1] + " " + dt.day.toString() + ", " + dt.year.toString(); 
+  }
+
+  void _openYoutube(String url) async{
+    if(await canLaunch(url))
+      await launch(url);
+  }
+
+  Future<Map> fetchArticleInfo(String url) async{
+    final postInfo = await http.get(url);
+    return json.decode(postInfo.body);;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'dart:convert';
+import 'ArticleView.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_html/flutter_html.dart';
-import 'package:html/dom.dart' as dom;
-import 'package:url_launcher/url_launcher.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'dart:convert';
 
 void main() => runApp(NASHUproar());
 
@@ -34,122 +30,64 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new ArticleView(
-      url: "https://nashuproar.org/wp-json/wp/v2/posts/14684?_embed",
-    );
-  }
-}
-
-class ArticleView extends StatefulWidget{
-  ArticleView({Key key, this.url}) : super(key: key);
-  final String url;
+  List _categories;
 
   @override
-  _ArticleViewState createState() => _ArticleViewState();
-}
-
-class _ArticleViewState extends State<ArticleView>{
-  Map _info; //the info about the article
-
-  @override
-  void initState() {
-    super.initState();
-    fetchArticleInfo(widget.url).then((Map m){
+  void initState() {     
+    _fetchCategories().then((List l){
       setState(() {
-        _info = m;
+        _categories = l;
       });
     });
   }
 
   @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title: Text("NASH Uproar"),
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: _categories == null ? 0 : _categories.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("NASH Uproar"),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs: _createTabs(),
+          ),
+        ),
+        body: _createBody(),
       ),
-      body: _createBody(),
     );
+  }
+
+  List<Tab> _createTabs(){
+    if(_categories == null)
+      return null;
+
+    //only create if the data has been loaded
+    //create a list of tabs
+    List<Tab> tabs = new List();
+    for(int i = 0; i < _categories.length; i++){
+      tabs.add(new Tab(
+        text: _categories[i]["name"],
+      ));
+    }
+
+    return tabs;
   }
 
   Widget _createBody(){
-    //Case: the information still needs to be loaded
-    if(_info == null){
-      return new Center(
+    //case 1: data hasn't loaded yet
+    if(_categories == null){
+      return Center(
         child: CircularProgressIndicator(),
       );
     }
+
+    //case 2: the data has already loaded, create the tabbar view
     
-    //Case: the information has already loaded
-    return new ListView(
-      children: <Widget>[
-        CachedNetworkImage(
-          imageUrl: _info["_embedded"]["wp:featuredmedia"][0]["media_details"]["sizes"]["medium"]["source_url"],
-        ),
-        Html(
-          data: _info["title"]["rendered"],
-        ), //title
-        Text(_getDate(DateTime.parse(_info["date"]))), //date
-        Text(_info["_embedded"]["author"][0]["name"]), //author
-        Html(
-          data: _info["content"]["rendered"],
-          customRender: (node, children){
-            if (node is dom.Element) {
-                switch (node.localName) {
-                  case "iframe":
-                    return new Container(
-                    width: MediaQuery.of(context).size.width * .75,
-                    child: Card(
-                      child: InkWell(
-                        onTap: () => _openYoutube(node.attributes["src"]),
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: <Widget>[
-                            CachedNetworkImage(
-                              width: MediaQuery.of(context).size.width * .75,
-                              imageUrl: "https://img.youtube.com/vi/" + node.attributes["src"].split("/")[node.attributes["src"].split("/").length - 1] + "/1.jpg",
-                              fit: BoxFit.cover,
-                            ),
-                            Icon(Icons.play_circle_filled, color: Colors.white,),
-                          ],
-                        ),
-                      ),
-                    ),
-                  );
-                }
-              }
-          },
-        ), //article content
-      ],
-    );
   }
 
-  String _getDate(DateTime dt){
-    List months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December"
-    ];
-    return months[dt.month - 1] + " " + dt.day.toString() + ", " + dt.year.toString(); 
-  }
-
-  void _openYoutube(String url) async{
-    if(await canLaunch(url))
-      await launch(url);
-  }
-
-  Future<Map> fetchArticleInfo(String url) async{
-    final postInfo = await http.get(url);
-    return json.decode(postInfo.body);;
+  Future<List> _fetchCategories() async{
+    final response = await http.get("https://raw.githubusercontent.com/akshathjain/nashuproar/master/categories.json");
+    return json.decode(response.body);
   }
 }
