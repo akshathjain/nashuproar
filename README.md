92c31132288285495d757f780c9e1f5152e9c7b8
Akshath Jain <akshath.r.jain@gmail.com>
2019-03-13 14:06:46 -0400

    added licenses to about page

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # NASH Uproar
 
-Flutter based Android & iOS application for the [NASH Uproar](https://nashuproar.org).
+### About the App
+- Flutter based Android & iOS application for the [NASH Uproar](https://nashuproar.org).
+- Pulls data using the Wordpress API 
+
+### Open Source Licenses
+
+### Screenshots
--- lib/AboutView.dart
+++ lib/AboutView.dart
@@ -7,7 +7,8 @@ Purpose: page that displays information about the app, developer, and open sourc
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:cached_network_image/cached_network_image.dart';
+import 'Colors.dart';
+
 
 class AboutView extends StatefulWidget{
   AboutView({Key key}) : super(key: key);
@@ -41,6 +42,8 @@ class _AboutViewState extends State<StatefulWidget>{
       body: ListView(
         padding: const EdgeInsets.fromLTRB(8.0, 24.0, 8.0, 12.0),
         children: <Widget>[
+          
+          
           Column( //about the app
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
@@ -55,7 +58,12 @@ class _AboutViewState extends State<StatefulWidget>{
               Text("Version: $_version"),
             ],
           ),
+          
+          
           SizedBox(height: 24.0,), //padding
+          
+          
+          //about the developer
           Card( // about the dev
             child: InkWell(
               onTap: () => _launchLink("https://akshathjain.com"),
@@ -72,19 +80,49 @@ class _AboutViewState extends State<StatefulWidget>{
               ),
             ),
           ),
+          
+          
           SizedBox(height: 24.0,), //padding
+          
+          
+          //open source licenses
           Card(
             child: Container(
               padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  _cardTitle("Open-Source Licenses"),
+
+                  _cardTitle("Open Source Licenses"),
+
+
+                  _link("cached_network_image", "https://github.com/renefloor/flutter_cached_network_image/blob/master/LICENSE.md"),
+                  _link("flutter_page_indicator", "https://github.com/best-flutter/flutter_page_indicator/blob/master/LICENSE"),
+                  _link("html", "https://github.com/dart-lang/html/blob/master/LICENSE"),
+                  _link("http", "https://github.com/dart-lang/http/blob/master/LICENSE"),
+                  _link("package_info", "https://github.com/flutter/plugins/blob/master/packages/package_info/LICENSE"),
+                  _link("share", "https://github.com/flutter/plugins/blob/master/packages/share/LICENSE"),
+                  _link("shared_preferences", "https://github.com/flutter/plugins/blob/master/packages/shared_preferences/LICENSE"),
+                  _link("url_launcher", "https://github.com/flutter/plugins/blob/master/packages/url_launcher/LICENSE"),
+
+
+                  RaisedButton(
+                    color: ACCENT_COLOR,
+                    child: Text("View All"),
+                    onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context){
+                      return new LicensePage();
+                    })),
+                  ),
                 ],
               ),
             ),
           ),
+          
+          
           SizedBox(height: 24.0,), //padding
+          
+          
+          //copyright
           Card(
             child: Container(
               padding: const EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 8.0),
@@ -104,12 +142,28 @@ class _AboutViewState extends State<StatefulWidget>{
 
   Widget _cardTitle(String text){
     return Container(
-      padding: const EdgeInsets.only(bottom: 4.0),
+      padding: const EdgeInsets.only(bottom: 8.0, top: 2.0),
       child: Text(
         text, 
         style: TextStyle(
           fontWeight: FontWeight.bold,
           fontSize: 18.0,
+        ),
+      )
+    );
+  }
+
+  Widget _link(String cover, String url){
+    return Container(
+      margin: const EdgeInsets.only(bottom: 4.0),
+      child: GestureDetector(
+        onTap: () => _launchLink(url),
+        child: Text(
+          cover,
+          style: TextStyle(
+            color: ACCENT_COLOR,
+            decoration: TextDecoration.underline,
+          ),
         ),
       )
     );
